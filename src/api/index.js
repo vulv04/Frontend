944d1a6b61462255ec315f3efc0693fc9ea61042
vulv04 <vulv04.it@gmail.com>
2025-07-08 21:44:33 +0700

    edit-baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://backend-b7ar.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
